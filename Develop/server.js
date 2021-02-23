cac86cdcdcb6e506372720d497ac7d0b4af52a79
Carly G <73192129+cgouge93@users.noreply.github.com>
2021-02-23 09:33:40 -0600

    started setting up server & routes

---
+++ Develop/server.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const app = express();
+const PORT = 8000;
+app.use(express.urlencoded({ extended: true}));
+app.use(express.json());
+
+require('./routes/apiRoutes')(app);
+require('./routes/htmlRoutes')(app);
