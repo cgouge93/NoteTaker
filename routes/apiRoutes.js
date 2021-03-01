ee0494d1d1de6d939f0289cd5b20e0ff2d036d85
cgouge93 <73192129+cgouge93@users.noreply.github.com>
2021-03-01 09:41:06 -0600

    Update apiRoutes.js

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,43 +1,53 @@
-const noteData = require('../db/db');
-const fs = require('fs');
-const path = require('path');
-module.exports = (app) => {
-    app.get('/api/notes', (req, res) => res.json(noteData));
-    app.post('/api/notes', (req, res) => {
-            let jsonFilePath = path.join(__dirname, "../db/db.json");
-            let newNote = req.body;
+const fs = require("fs");
+var data = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+
+
+module.exports = function(app) {
+
+    app.get("/api/notes", function(req, res) {
+       
+        res.json(data);
+
+    });
+
+    app.get("/api/notes/:id", function(req, res) {
+
+        res.json(data[Number(req.params.id)]);
+
+    });
+
+
+    app.post("/api/notes", function(req, res) {
+
+        let newNote = req.body;
+        let uniqueId = (data.length).toString();
+        console.log(uniqueId);
+        newNote.id = uniqueId;
+        data.push(newNote);
+        
+        fs.writeFileSync("./db/db.json", JSON.stringify(data), function(err) {
+            if (err) throw (err);        
+        }); 
+
+        res.json(data);    
+
+    });
+
     
-            // This allows the test note to be the original note.
-            let highestId = 99;
-            // This loops through the array and finds the highest ID.
-            for (let i = 0; i < noteData.length; i++) {
-                let individualNote = noteData[i];
-    
-                if (individualNote.id > highestId) {
-                    // highestId will always be the highest numbered id in the notesArray.
-                    highestId = individualNote.id;
-                }
-            }
-            // This assigns an ID to the newNote. 
-            newNote.id = highestId + 1;
-            // We push it to db.json.
-            noteData.push(newNote)
-    
-            // Write the db.json file again.
-            fs.writeFile(jsonFilePath, JSON.stringify(noteData), function (err) {
-    
-                if (err) {
-                    return console.log(err);
-                }
-                console.log("Your note was saved!");
-            });
-            // Gives back the response, which is the user's new note. 
-            res.json(newNote);
+    app.delete("/api/notes/:id", function(req, res) {
+
+        let noteId = req.params.id;
+        let newId = 0;
+        console.log(`Deleting note with id ${noteId}`);
+        data = data.filter(currentNote => {
+           return currentNote.id != noteId;
         });
-    ;
-    app.delete('/api/notes/:id', (req, res) => {
-        let note = noteData.find( ({id}) => id === JSON.parse(req.params.id));
-        noteData.splice(noteData.indexOf(note), 1);
-        res.end("Note deleted")
-    })
+        for (currentNote of data) {
+            currentNote.id = newId.toString();
+            newId++;
+        }
+        fs.writeFileSync("./db/db.json", JSON.stringify(data));
+        res.json(data);
+    }); 
+
 }
